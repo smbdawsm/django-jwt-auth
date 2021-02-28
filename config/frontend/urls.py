3c22a50b04b6f5e7b6d24d3c921e1e6e6519cddf
Nikolay Nikishev <nikolay.nikishev@icloud.com>
2021-02-28 10:17:24 +0700

    first commit

---
+++ config/frontend/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.urls import path
+from .views import index as index_view
+
+urlpatterns = [
+    path('', index_view),  # for the empty url
+    url(r'^.*/$', index_view)  # for all other urls
+]
